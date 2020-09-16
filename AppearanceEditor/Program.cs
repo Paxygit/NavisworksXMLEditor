be0802ae8d7b4074969a1611698b1c66dc22be35
Frank D. Kuenneke <fdkuenneke@armistead-nj.com>
2020-09-16 13:20:26 -0400

    XML in working state, just need work to assemble. Paths class is private for..... privacy reasons?

--- AppearanceEditor/Program.cs
+++ AppearanceEditor/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Xml;
 
 namespace AppearanceEditor
 {
     class Program
     {
-        static void Main(string[] args)
+        static readonly string xmlPath = @"C:\Users\fdkuenneke\OneDrive - Armistead Mechanical Inc\Desktop\Armistead Services.xml";
+
+        static readonly string savePath = @"C:\Users\fdkuenneke\OneDrive - Armistead Mechanical Inc\Desktop\Armistead Services Test.xml";
+            
+            
+            static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            xmlLoader();
+        }
+        public static void xmlLoader()
+        {
+
+            XmlDocument xml = new XmlDocument();
+            xml.Load(xmlPath);
+            XmlNode newNode = xml.CreateNode(XmlNodeType.Element, "selectionset", "third createnode var");
+            newNode.InnerText = "TESTTESTTESTTEST";
+
+            XmlNode findspec = xml["exchange"]["selectionsets"]["viewfolder"];
+            XmlElement groups = findspec["selectionset"];
+            findspec.InsertAfter(newNode, groups);
+            xml.Save(savePath);
+            
+            
+            
+            
+          //  var query = from c in xml.Descendants("data")
+          //              select (string)c.Value;
+          //  foreach (string name in query)
+          //  {                                                  Formatting test for later reference
+          //      Console.WriteLine("Found: {0}", name);
+          //  }
         }
     }
 }
